282836e8b80ee856fd5c2c2ac55191d058585217
sugam <sugamsingh377@gmail.com>
2024-12-02 14:29:10 +0100

    Added Seed Data

--- ProMgt/Data/ProjectDbContext.cs
+++ ProMgt/Data/ProjectDbContext.cs
@@ -28,33 +28,33 @@ namespace ProMgt.Data
             base.OnModelCreating(modelBuilder);
 
             ////Seed ProjectMgtColors
-            //modelBuilder.Entity<ProjectMgtColor>().HasData(
-            //    new ProjectMgtColor { Id = 1, Name = "Light Gray", HexCode = "#C7C4C4" },
-            //    new ProjectMgtColor { Id = 2, Name = "Soft Red", HexCode = "#EF6A6A" },
-            //    new ProjectMgtColor { Id = 3, Name = "Peach", HexCode = "#EC8D71" },
-            //    new ProjectMgtColor { Id = 4, Name = "Light Orange", HexCode = "#F1BD6C" },
-            //    new ProjectMgtColor { Id = 5, Name = "Pale Yellow", HexCode = "#F2DA6F" },
-            //    new ProjectMgtColor { Id = 6, Name = "Light Green", HexCode = "#B3DF97" },
-            //    new ProjectMgtColor { Id = 7, Name = "Mint Green", HexCode = "#83C9A8" },
-            //    new ProjectMgtColor { Id = 8, Name = "Aqua", HexCode = "#4ECBC4" },
-            //    new ProjectMgtColor { Id = 9, Name = "Light Cyan", HexCode = "#9EE7E3" },
-            //    new ProjectMgtColor { Id = 10, Name = "Blue", HexCode = "#4573D2" },
-            //    new ProjectMgtColor { Id = 11, Name = "Lavender", HexCode = "#A79FF3" },
-            //    new ProjectMgtColor { Id = 12, Name = "Light Purple", HexCode = "#C48FDF" },
-            //    new ProjectMgtColor { Id = 13, Name = "Pink", HexCode = "#F9AAEF" },
-            //    new ProjectMgtColor { Id = 14, Name = "Hot Pink", HexCode = "#F26FB2" },
-            //    new ProjectMgtColor { Id = 15, Name = "Salmon", HexCode = "#FC979A" },
-            //    new ProjectMgtColor { Id = 16, Name = "Charcoal Gray", HexCode = "#6D6E6F" }
-            //);
+            modelBuilder.Entity<ProjectMgtColor>().HasData(
+                new ProjectMgtColor { Id = 1, Name = "Light Gray", HexCode = "#C7C4C4" },
+                new ProjectMgtColor { Id = 2, Name = "Soft Red", HexCode = "#EF6A6A" },
+                new ProjectMgtColor { Id = 3, Name = "Peach", HexCode = "#EC8D71" },
+                new ProjectMgtColor { Id = 4, Name = "Light Orange", HexCode = "#F1BD6C" },
+                new ProjectMgtColor { Id = 5, Name = "Pale Yellow", HexCode = "#F2DA6F" },
+                new ProjectMgtColor { Id = 6, Name = "Light Green", HexCode = "#B3DF97" },
+                new ProjectMgtColor { Id = 7, Name = "Mint Green", HexCode = "#83C9A8" },
+                new ProjectMgtColor { Id = 8, Name = "Aqua", HexCode = "#4ECBC4" },
+                new ProjectMgtColor { Id = 9, Name = "Light Cyan", HexCode = "#9EE7E3" },
+                new ProjectMgtColor { Id = 10, Name = "Blue", HexCode = "#4573D2" },
+                new ProjectMgtColor { Id = 11, Name = "Lavender", HexCode = "#A79FF3" },
+                new ProjectMgtColor { Id = 12, Name = "Light Purple", HexCode = "#C48FDF" },
+                new ProjectMgtColor { Id = 13, Name = "Pink", HexCode = "#F9AAEF" },
+                new ProjectMgtColor { Id = 14, Name = "Hot Pink", HexCode = "#F26FB2" },
+                new ProjectMgtColor { Id = 15, Name = "Salmon", HexCode = "#FC979A" },
+                new ProjectMgtColor { Id = 16, Name = "Charcoal Gray", HexCode = "#6D6E6F" }
+            );
 
             ////Seed ProjectStatuses
-            //modelBuilder.Entity<ProjectStatus>().HasData(
-            //    new ProjectStatus { Id = 1, Name = "On track", HexCode = "#4573D2" },
-            //    new ProjectStatus { Id = 2, Name = "At Risk", HexCode = "#EF6A6A" },
-            //    new ProjectStatus { Id = 3, Name = "Off track", HexCode = "#F26FB2" },
-            //    new ProjectStatus { Id = 4, Name = "On hold", HexCode = "#F2DA6F" },
-            //    new ProjectStatus { Id = 5, Name = "Completed", HexCode = "#83C9A8" }
-            //);
+            modelBuilder.Entity<ProjectStatus>().HasData(
+                new ProjectStatus { Id = 1, Name = "On track", HexCode = "#4573D2" },
+                new ProjectStatus { Id = 2, Name = "At Risk", HexCode = "#EF6A6A" },
+                new ProjectStatus { Id = 3, Name = "Off track", HexCode = "#F26FB2" },
+                new ProjectStatus { Id = 4, Name = "On hold", HexCode = "#F2DA6F" },
+                new ProjectStatus { Id = 5, Name = "Completed", HexCode = "#83C9A8" }
+            );
 
             modelBuilder.Entity<Project>()
                 .HasMany(p => p.Tasks)
